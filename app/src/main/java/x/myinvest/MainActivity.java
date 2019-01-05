787ca8785f9726402204c5fc2f120ad9fc81982c
myyfan <77558986@qq.com>
2019-01-05 21:09:20 +0800

    add

--- app/src/main/java/x/myinvest/MainActivity.java
+++ app/src/main/java/x/myinvest/MainActivity.java
@@ -1,13 +1,22 @@
 package x.myinvest;
 
+import android.content.SharedPreferences;
+import android.print.PrinterId;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private  SharedPreferences buyedStock=null;
+    private  String buyedStockCode=getApplicationContext().getSharedPreferences("buyedStockCode",0).getString("buyedStockCode","");
+    private  String buyedStockPrice=getApplicationContext().getSharedPreferences("buyedStockCode",0).getString("buyedStockCode","");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
