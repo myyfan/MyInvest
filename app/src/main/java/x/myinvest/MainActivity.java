5074ff73bdefe9bd45403f23a51509ac45f28b43
myyfan <myyfan@126.com>
2019-02-25 16:58:19 +0800

    添加菜单

--- app/src/main/java/x/myinvest/MainActivity.java
+++ app/src/main/java/x/myinvest/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -65,6 +67,13 @@ public class MainActivity extends AppCompatActivity {
         updateTabView();
         pullNetworkData();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(this);
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
