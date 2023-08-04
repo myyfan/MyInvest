a5e6944d0b9bc13358846d67ce214d2cf3a9f8a4
X <77558986@qq.com>
2023-08-04 11:41:52 +0800

    输入分红数据时，不再刷新持股视图；修复修改历史上未记录数据的盈利情况的功能；

--- app/src/main/java/x/myinvest/popup/PopupChangeGained.java
+++ app/src/main/java/x/myinvest/popup/PopupChangeGained.java
@@ -15,18 +15,20 @@ public class PopupChangeGained extends LinearLayout {
     TextView textViewGained;
     Button bnOk;
     Context context;
-    public PopupChangeGained(MainActivity context) {
+    public PopupChangeGained(MainActivity context,double gained) {
         super(context);
         this.context = context;
         LayoutInflater.from(context).inflate(R.layout.popup_change_gained, this);
         textViewGained = (EditText) findViewById(R.id.layout_changeGained_editText_changeGained);
         bnOk = (Button) findViewById(R.id.layout_changeGained_button_ok);
-        textViewGained.setText(context.gained+"");
+        textViewGained.setText(gained+"");
         bnOk.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double gained = Double.parseDouble(textViewGained.getText().toString());
-                ((MainActivity)context).changeGained(gained);
+                Double gained1 = Double.parseDouble(textViewGained.getText().toString());
+                ((MainActivity)context).changeGained(gained1);
+                context.gained=context.gained-gained+gained1;
+           //     context.loadSavedData();
             }
         });
     }
