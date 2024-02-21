383551a0be3af51143bac85f4b53e269baa9e7c7
X <77558986@qq.com>
2024-02-21 14:33:39 +0800

    估计市场数据弹窗增加市场收益率估算

--- app/src/main/java/x/myinvest/MainActivity.java
+++ app/src/main/java/x/myinvest/MainActivity.java
@@ -529,6 +529,10 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("预估PE");
         tableRow.addView(textView);
 
+        textView=new TextView(this);
+        textView.setText("预估收益");
+        tableRow.addView(textView);
+
         tableLayout.addView(tableRow, layoutParams);
 
         Double [] yuCeCangWeiTable={0.0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0,1.05,1.10};
@@ -583,6 +587,11 @@ public class MainActivity extends AppCompatActivity {
             //预估目标仓位的PE
             textView=new TextView(this);
             textView.setText(String.format("%.2f", yuJiPE));
+            tableRow.addView(textView);
+
+            //预估目标仓位的收益率
+            textView=new TextView(this);
+            textView.setText(String.format("%.2f", 100/yuJiPE)+"%");
             tableRow.addView(textView);
 
             tableLayout.addView(tableRow, layoutParams);
